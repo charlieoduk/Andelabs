e20bf269c2752e028b6201e4c9c9f710622aeb2f
charlieoduk <odukjr@gmail.com>
2017-05-10 11:02:56 +0300

    updated the first if statement

--- Day 3/fizz_buzz.py
+++ Day 3/fizz_buzz.py
@@ -1,9 +1,9 @@
 def fizz_buzz(num):
-	if (num % 3) and (num % 5) = 0:
-		return "Fizz"
-	elif num % 5 == 0:
-		return "Buzz" 
-	elif num % 3 == 0:
+	if (num % 3)== 0 and (num % 5) == 0:
 		return "FizzBuzz"
+	elif num % 5 == 0:
+		return "Buzz"
+	elif num % 3 == 0:
+		return "Fizz"
 	else:
 		return num

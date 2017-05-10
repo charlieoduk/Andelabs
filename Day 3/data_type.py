1f48b21128ef7ab5bc34904a992e243cb2a96d89
charlieoduk <odukjr@gmail.com>
2017-05-10 16:32:50 +0300

    revised

--- Day 3/data_type.py
+++ Day 3/data_type.py
@@ -1,20 +1,25 @@
 def data_type(something):
-	is_type = type(something)
-	if is_type == str:
-		length = len(something)
-		return length
-	elif is_type == None:
-		return "no value"
-	elif is_type == bool:
-		return something
-	elif is_type == int:
-		if something < 100:
-			return "less than 100"
-		elif something == 100:
-			return "equal to 100"
-		else:
-			return "more than 100"
-	elif is_type == list:
-         if (len(something) < 3):
-           return None
-	return something[2]
+    accepted_input = [str,None,bool,int,list]
+    valid = isinstance(accepted_input)
+    if something is valid:
+        is_type = type(something)
+        if is_type ==str:
+            length = len(something)
+            return length
+        elif is_type == None:
+            return 'no value'
+        elif is_type == bool:
+            return something
+        elif is_type == int:
+            if something < 100:
+                return 'less than 100'
+            elif something == 100:
+                return 'equal to 100'
+            else:
+                return 'more than 100'
+        elif is_type == list:
+            if (len(something) < 3):
+                return None
+        return None
+    else:
+        raise ValueError
--- Day 3/wordcount.py
+++ Day 3/wordcount.py
@@ -1,8 +1,16 @@
-def countwords(str):
-	d = {}
-	split_str = str.split(' ')
+import collections
+from collections import Counter
 
-	for i in split_str:
-		d[i] = split_str.count(i)
-
-	return d
+def words(string):
+    if type(string) == str:
+        split_string = phrase.split()
+        word_occurence = dict(Counter(split_string))
+        for key in word_occurence.keys():
+            try:
+                int(key)
+                word_occurence[int(key)] = word_occurence.pop(key)
+            except ValueError:
+                continue
+        return word_occurence
+    else:
+        raise TypeError

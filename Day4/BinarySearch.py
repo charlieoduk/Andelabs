b204454fd6979848d0e964eeac674fc78bfb38ee
charlieoduk <odukjr@gmail.com>
2017-05-11 17:23:52 +0300

    Used a while loop instead of a recursive function for search

--- Day4/BinarySearch.py
+++ Day4/BinarySearch.py
@@ -1,26 +1,42 @@
 class BinarySearch(list):
-	"""docstring for BinarySearch"""
-	def __init__(self, a, b):
-		super().__init__()
-		self.length = a
-		self.b = b
+    """BinarySearch class
+    """
+    def __init__(self, a, b):
+        super().__init__()
 
-		for i in range(self.length):
-			list.append(self,self.b)
-			self.b += b
+        for i in range(1, a+1):
+            self.append(i * b)
 
+        self.length = a
 
-	def search(self, num):
-		def binary(self, list, num):
-			midpoint = len(list)//2
-			count = 0
-			if list[midpoint] == num:
-				return {'count':count,'index':midpoint}
-			else:
-				if num < list[midpoint]:
-					count += 1
-					return binary(list[:midpoint], num)
-				else:
-					count += 1
-					return binary(list[midpoint+1:], num)
-		
+    def search(self, num):
+        first = 0
+        last = len(self) - 1
+        num_index = 0
+        found = False
+        counter = 0
+
+        if num == self[first]:
+            num_index = first
+            found = True
+        elif num == self[last]:
+            num_index = last
+            found = True
+
+        if num not in self:
+            found = True
+            num_index = -1
+
+        while first <= last and not found:
+            mid = (first + last) // 2
+            if self[mid] == num:
+                found = True
+                num_index = mid
+            else:
+                counter += 1
+                if num < self[mid]:
+                    last = mid - 1
+                else:
+                    first = mid + 1
+
+        return {'count': counter, 'index': num_index}
